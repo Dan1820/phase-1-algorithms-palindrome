a72b83c376acd8d6aa229d4558d9e42d8d2c2551
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-04-02 16:51:41 +0300

    completed message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function isPalindrome(word) {
   // Write your algorithm here
   let lowerCaseWord= word.toLowerCase()
-  let reverseWord= lowerCaseWord.reverse()
+  let reverseWord= lowerCaseWord.split("").reverse().join("")
   if (reverseWord===lowerCaseWord){
     return true
   }else{
@@ -16,10 +16,10 @@ console.log(isPalindrome("mom"))
   use the toLowerCase() method to convert the string to lowercase
   iterate over each letter in the string
   perform reverse() method on the string
-  if the word is a palindrome
+  if the word is the same as the reversed word
   return true
   else return false 
-  if the word is not a palindrome
+  
 
 */
 
@@ -34,10 +34,19 @@ console.log(isPalindrome("mom"))
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  it("returns LowerCase", ()=>{
+     word.toLowerCase()
+  })
+  it("return reverse ",()=>{
+    word.reverse()
+  })
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("r"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
